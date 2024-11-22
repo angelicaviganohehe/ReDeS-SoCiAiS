f2767be45459cdb1798f9ac8494574a5ea8dbd7e
angelicaviganohehe <106531341+angelicaviganohehe@users.noreply.github.com>
2024-11-22 11:44:55 -0300

    Add files via upload

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 async function quantidadeUsuarios() {
 const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
 const res = await fetch(url)
@@ -23,16 +23,47 @@ const data = [
     
     ]
 
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+         text: 'Redes sociais com mais usuários no mundo',
+         x: 0,
+         font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
+         }
+     },
+     xaxis:{
+      tickfont: tickConfig,
+         
+      title: {
+         text: 'nome das redes sociais',
+         font: {
+         color: getCSS('--secundary-color')
+         }
+         }
+     },
+     yaxis: {
+         tickfont: tickConfig,
+         title: {
+         text: 'bilhões de usuários ativos',
+         font: {
+         color: getCSS('--secundary-color')
+         }
+         }
+         }
+     }
+     
+
     
 const grafico = document.createElement('div')
 grafico.className = 'grafico'
-document.getElementById('graficos-container').appendChild(grafico)
-Plotly.newPlot(grafico, data, layout )
 
-const layout = {
-   plot_bgcolor: getCSS('--bg-color'),
-   paper_bgcolor: getCSS('--bg-color')
-     }
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data, layout)
+
 
 }
 
